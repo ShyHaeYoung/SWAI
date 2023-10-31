99a6a5001114d5364b539807f3f48ee8a2ecb401
rlawndks4 <rlawndks0423@naver.com>
2023-11-01 02:11:29 +0900

    feat: udpate

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -6,6 +6,7 @@ import { Doughnut, Bar } from 'react-chartjs-2';
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import axios from "axios";
 
 ChartJS.register(
     ArcElement,
@@ -21,11 +22,12 @@ const RowWrappers = styled.div`
 display: flex;
 width: 100%;
 `
-const Video = styled.div`
-width: 20vw;
-height: 20vh;
-background-color: blue;
-color: #fff;
+const BorderContainer = styled.div`
+width: 40vw;
+height: 30vh;
+display: flex;
+border:1px solid #ccc;
+flex-wrap: wrap;
 `
 const ChartContainer = styled.div`
 
@@ -42,6 +44,11 @@ justify-content: space-between;
 const DonutContainer = styled.div`
 width: 40%;
 `
+const DataSection = styled.div`
+border: 1px solid #ccc;
+height: 100%;
+display: flex;
+`
 const Result = () => {
 
     const location = useLocation();
@@ -51,8 +58,8 @@ const Result = () => {
         labelSuffix: "%",
         datasets: [
             {
-                label: '# of Votes',
-                data: [12, 14],
+                label: '차량 정보',
+                data: [],
                 backgroundColor: ['#f7efef', '#f8e0df', '#f6c6c4', '#f5ae8f', '#f2c096', '#e6a975', '#f7c15f', '#ffa700', '#ec8733', '#f06d00'],
                 borderColor: ['#f7efef', '#f8e0df', '#f6c6c4', '#f5ae8f', '#f2c096', '#e6a975', '#f7c15f', '#ffa700', '#ec8733', '#f06d00'],
                 borderWidth: 1,
@@ -68,7 +75,7 @@ const Result = () => {
                 backgroundColor: function (context) {
                     return context.dataset.backgroundColor;
                 },
-                formatter: (val, context) => `${val}%`,
+                formatter: (val, context) => `${val}ㄴ개`,
                 borderRadius: 25,
                 borderWidth: 3,
                 color: "black",
@@ -79,66 +86,84 @@ const Result = () => {
             },
             tooltip: {
                 callbacks: {
-                    label: (ttItem) => `${ttItem.label}: ${ttItem.parsed}%`
+                    label: (ttItem) => `${ttItem.parsed}개`
                 }
             }
         }
     }
-    const bar_options = {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: 'Chart.js Bar Chart',
-            },
-        },
-    }
-
-    const bar_data = {
-        labels: ['#1', '#2'],
-        datasets: [
-            {
-                label: 'Dataset 1',
-                data: [12, 36],
-                backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            },
-        ]
-    };
 
     const [donutObj, setDonutObj] = useState(donut_data ?? {})
-    const [barObj, setBarObj] = useState(bar_data ?? {})
-    const [carObj, setCarObj] = useState({})
-
+    const [carData, setCarData] = useState({});
     useEffect(() => {
-        setCarObj({ ...location.state })
+        getResultData();
     }, [])
+
+    const getResultData = async () => {
+        const { data: response } = await axios.get(`/api/dummy-data/${location.state?.carNumber}`);
+        console.log(response);
+        setCarData(response);
+        let car_donut_list = [];
+        car_donut_list.push(response?.scratch)
+        car_donut_list.push(response?.installation)
+        car_donut_list.push(response?.exterior)
+        car_donut_list.push(response?.gap)
+        setDonutObj({
+            ...donut_data,
+            labels: ['스크래치', '장착불량', '외관손상', '단차'],
+            datasets: [
+                {
+                    ...donut_data.datasets[0],
+                    data: car_donut_list
+                },
+
+            ],
+        });
+
+    }
     return (
         <>
             <RowWrappers>
                 <Col style={{ backgroundImage: `url(${SideBannerImg})`, height: '100vh', width: '20vw', color: '#fff', fontWeight: 'bold' }}>
                     <Col style={{ margin: 'auto', alignItems: 'center', rowGap: '0.5rem' }}>
-                        <Text2>{carObj?.carNumber}</Text2>
+                        <Text2>{carData?.carNumber}</Text2>
                         <Text2>검사결과</Text2>
                         <Text2>VIEW</Text2>
                     </Col>
-
                 </Col>
                 <ContentContainer>
-                    <Row style={{ justifyContent: 'space-between' }}>
-                        <Video>비디오</Video>
-                        <ChartContainer>
-                            <Bar data={barObj.labels.length > 0 ? barObj : bar_data} options={bar_options} />
-                        </ChartContainer>
-                    </Row>
-                    <BottmChartContainer>
+                    <Row style={{ justifyContent: 'space-between', alignItems: 'center' }}>
+                        <BorderContainer>
+                            {[1, 2, 3, 4, 5, 6, 7, 8].map((image, idx) => (
+                                <>
+                                    <Col style={{ width: `${100 / 4}%` }}>
+                                        <DataSection>
+                                            <div style={{ margin: 'auto' }}>
+                                                사진 {idx + 1}
+                                            </div>
+                                        </DataSection>
+                                    </Col>
+                                </>
+                            ))}
+                        </BorderContainer>
                         <DonutContainer>
-                        <Doughnut data={donutObj.labels.length > 0 ? donutObj : donut_data} options={donut_options} />
+                            <Doughnut data={donutObj.labels.length > 0 ? donutObj : donut_data} options={donut_options} />
 
                         </DonutContainer>
-                    </BottmChartContainer>
+                    </Row>
+                    <Row style={{ justifyContent: 'space-between', alignItems: 'center', columnGap: '1rem' }}>
+                        <BorderContainer >
+                            <Col style={{ margin: 'auto', rowGap: '0.5rem' }}>
+                                <div>스크래치: {carData?.scratch}</div>
+                                <div>장착불량: {carData?.installation}</div>
+                                <div>외관손상: {carData?.exterior}</div>
+                                <div>단차: {carData?.gap}</div>
+                                <div>총 불량개수: {carData?.totalDefects}</div>
+                            </Col>
+                        </BorderContainer>
+                        <BorderContainer>
+                            <div style={{ margin: 'auto' }}>미구현</div>
+                        </BorderContainer>
+                    </Row>
                 </ContentContainer>
             </RowWrappers>
         </>
